6e307c72e25b113e22955edcd26906cac3a3002b
MB <michal@boruch.xyz>
2020-11-14 00:01:14 +0100

    Add dummy endpoint.

---
+++ django/app/foo/views.py
@@ -0,0 +1,5 @@
+from django.http import JsonResponse
+
+
+def foo(request):
+    return JsonResponse({'foo':'bar'})
--- django/app/root/settings.py
+++ django/app/root/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'foo',
 ]
 
 MIDDLEWARE = [
@@ -117,4 +119,4 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/api/static/'
